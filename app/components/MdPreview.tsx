e91b795aa42fc89b0f88be10856404ad18531118
佳文 <nbsawz@gmail.com>
2019-05-20 18:53:25 +0800

    [wip] scroll sync

--- app/components/MdPreview.tsx
+++ app/components/MdPreview.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
 import MarkdownIt from 'markdown-it';
+import styled from 'styled-components';
 
 interface Props {
   content: string;
   r: React.RefObject<HTMLDivElement>;
 }
 
+const Preview = styled.div`
+  overflow: auto;
+  height: 100%;
+  box-sizing: border-box;
+`;
+
 class MdPreview extends React.Component<Props> {
   md = MarkdownIt();
   render() {
     const { content: html, r } = this.props;
     return (
-      <div
+      <Preview
         ref={r}
         className="markdown-body"
         dangerouslySetInnerHTML={{ __html: this.md.render(html) }}
