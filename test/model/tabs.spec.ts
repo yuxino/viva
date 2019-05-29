83e229a3dcc7a81e68d5c47e81043f5168d09f9f
佳文 <nbsawz@gmail.com>
2019-05-29 18:22:05 +0800

    [wip] add tab test case

---
+++ test/model/tabs.spec.ts
@@ -0,0 +1,44 @@
+// import tabs from "../../app/model/tabs"
+
+describe('model/tabs', () => {
+  it('should have description', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  describe('addTab', () => {
+    it('should add a new tab in tab list', () => {
+
+    })
+  })
+
+  describe("removeTab", () => {
+    it('should remove a tab by instance', () => {
+
+    })
+  })
+
+  describe("removeSavedTab", () => {
+    it('should remove all saved tabs', () => {
+
+    })
+  })
+
+  describe("removeAll", () => {
+    it('should remove all tabs',() => {
+
+    })
+  })
+
+  describe("removeLeft", () => {
+    it('should remove all tabs', () => {
+
+    })
+  })
+
+
+  describe("removeRight", () => {
+    it('should remove all tabs', () => {
+
+    })
+  })
+});
