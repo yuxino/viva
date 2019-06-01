1582be0d5adad10135bd635c108d0237e300257a
佳文 <nbsawz@gmail.com>
2019-06-01 17:51:30 +0800

    tabs swap function done

--- app/model/tabs.ts
+++ app/model/tabs.ts
@@ -109,14 +109,87 @@ export default class Tabs {
     return this.head;
   }
 
+  private swapBetween(tab, tab2) {
+    const prevTab = tab.prev;
+    const nextTab = tab.next;
+
+    const prevTab2 = tab2.prev;
+    const nextTab2 = tab2.next;
+
+    const tempTab = new Tab(tab);
+    const tempTab2 = new Tab(tab2);
+    // near ...
+    // prevTab <-> tempTab2 <-> tempTab2 <-> nextTab2
+    if (tab === prevTab2) {
+      prevTab.next = tempTab2;
+      nextTab2.prev = tempTab;
+
+      tempTab2.prev = prevTab;
+      tempTab2.next = tempTab;
+
+      tempTab.prev = tempTab2;
+      tempTab.next = nextTab2;
+    } else {
+      // prevTab <-> tempTab2 <-> nextTab
+      prevTab.next = tempTab2; // 1 -> 2
+      nextTab.prev = tempTab2; //
+      tempTab2.prev = prevTab;
+      tempTab2.next = nextTab;
+
+      // prevTab2 <-> tempTab <-> nextTab2
+      prevTab2.next = tempTab;
+      nextTab2.prev = tempTab;
+      tempTab.prev = prevTab2;
+      tempTab.next = nextTab2;
+    }
+  }
+
+  private swapHeadTail() {
+    const temp = this._tail;
+    this._tail = this._head;
+    this._tail.next = null;
+    this._tail.prev = temp;
+
+    this._head = temp;
+    this._head.prev = null;
+    this._head.next = this._tail;
+  }
+
+  private swapwithHead(tab) {
+    this._head.next = tab.next;
+    this._head.prev = tab;
+
+    tab.prev = null;
+    tab.next = this._head;
+    this._head = tab;
+  }
+
+  private swapWithTail(tab) {
+    tab.prev.next = this.tail;
+    this.tail.prev = tab.prev;
+
+    this._tail.next = tab;
+    this._tail = tab;
+    tab.next = null;
+  }
+
   public swapTab(tab, tab2) {
-    // same tab .. don't swap
     if (tab === tab2) return;
     const tabIsHead = this._isHead(tab);
     const tab2IsHead = this._isHead(tab2);
-
     const tabIsTail = this._isTail(tab);
-    const tab2isTail = this._isTail(tab2);
+    const tab2IsTail = this._isTail(tab2);
+    const bothInMid = !tabIsHead && !tabIsTail && !tab2IsHead && !tab2IsTail;
+
+    if (bothInMid) {
+      this.swapBetween(tab, tab2);
+    } else if ((tabIsHead && tab2IsTail) || (tab2IsHead && tabIsTail)) {
+      this.swapHeadTail();
+    } else if (tabIsHead || tab2IsHead) {
+      this.swapwithHead(tabIsHead ? tab2 : tab);
+    } else if (tabIsTail || tab2IsTail) {
+      this.swapWithTail(tabIsTail ? tab2 : tab);
+    }
     return this.head;
   }
 
