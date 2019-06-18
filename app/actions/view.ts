00244c626ceea7e11f42c0c15201e123e651456a
gavin <nbsawz@gmail.com>
2019-06-18 18:51:48 +0800

    refactor file upload part

--- app/actions/view.ts
+++ app/actions/view.ts
@@ -1,7 +1,8 @@
 enum View {
   ALL_UPDATE = 'VIEW_ALL_UPDATE', // update all views
   TABS_UPDATE = 'VIEW_TABS_UPDATE', // update tabs view
-  EXPLORE_UPDATE = 'VIEW_EXPLORE_UPDATE' // update explore view
+  EXPLORE_UPDATE = 'VIEW_EXPLORE_UPDATE', // update explore view
+  UPDATE_EDITOR_SYNC_FN = 'VIEW_UPDATE_EDITOR_SYNC_FN' // update sync function
 }
 
 export default View;
--- app/reducers/view.ts
+++ app/reducers/view.ts
@@ -1,10 +1,18 @@
 import produce from 'immer';
+import ViewActions from '../actions/view';
 
 export const initState = function() {
-  return {};
+  return {
+    edtiorSyncFn: null
+  };
 };
 
 export default (state = initState(), action) =>
   produce(state, draft => {
-    // do sth ..
+    switch (action.type) {
+      case ViewActions.UPDATE_EDITOR_SYNC_FN: {
+        draft.edtiorSyncFn = action.payload.edtiorSyncFn;
+        break;
+      }
+    }
   });
