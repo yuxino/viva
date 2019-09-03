0e251dd4e0853fdf6768626b87f50db570824868
yuxino <nbsawz@gmail.com>
2019-09-03 11:17:43 +0800

    refactor: refactor container part

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,18 +1,8 @@
 import * as React from 'react';
-import { useRef, useState, useEffect } from 'react';
-import {
-  Layout,
-  MdEditor,
-  MdPreview,
-  VivaTitleBar,
-  Tabs,
-  WorkBench
-  // WorkBench
-} from '../components';
-import { useDispatch } from 'redux-react-hook';
-import ViewActions from '../actions/View';
+import { Layout, VivaTitleBar, Tabs, WorkBench } from '../components';
 import FileDrop from '../dnd/FileDrop';
 import H5DnD from '../dnd/h5DnD';
+import EditorPreview from './EditorPreview';
 
 /**
  * APP Layout ⤵️
@@ -24,55 +14,10 @@ import H5DnD from '../dnd/h5DnD';
        |> bottomIconGroup
  * |>  main
  *     |> tabs
- *     |> container
- *       |> left
- *         |> MdEditor
- *       |> right
- *         |> MdPreview
+ *     |> EditorPreview | Bootstrap
  */
 
 function App() {
-  const dispath = useDispatch();
-  const updateEditorSyncFn = payload =>
-    dispath({ type: ViewActions.UPDATE_EDITOR_SYNC_FN, payload });
-
-  const [content, setContent] = useState('');
-
-  const editorRef = useRef<HTMLDivElement>();
-  const previewRef = useRef<HTMLDivElement>();
-
-  // handle editor input
-  const inputHandle = () => {
-    const editor = editorRef.current;
-    setContent(editor.innerText);
-  };
-
-  const edtiorSyncFn = content => {
-    const editor = editorRef.current;
-    editor.innerText = content;
-    setContent(content);
-  };
-
-  const scrollHanlder = e => {
-    const editor = editorRef.current;
-    const preview = previewRef.current;
-
-    // scroll target
-    const t1 = e.target === editor ? editor : preview;
-    const t2 = t1 === editor ? preview : editor;
-
-    setTimeout(() => {
-      const percentage = t1.scrollTop / (t1.scrollHeight - t1.offsetHeight);
-      const height = percentage * (t2.scrollHeight - t2.offsetHeight);
-      t2.scrollTop = height;
-    }, 0);
-  };
-
-  // async editor ref in reducer
-  useEffect(() => {
-    updateEditorSyncFn({ edtiorSyncFn });
-  }, [editorRef]);
-
   return (
     <FileDrop>
       <Layout>
@@ -84,23 +29,7 @@ function App() {
           <WorkBench>213</WorkBench>
           <Layout.View>
             <Tabs />
-            <Layout.Container>
-              <Layout.Left>
-                <MdEditor
-                  ref={editorRef}
-                  onInput={inputHandle}
-                  onScroll={scrollHanlder}
-                  placeholder="type something here ..."
-                />
-              </Layout.Left>
-              <Layout.Right>
-                <MdPreview
-                  content={content}
-                  ref={previewRef}
-                  onScroll={scrollHanlder}
-                />
-              </Layout.Right>
-            </Layout.Container>
+            <EditorPreview />
           </Layout.View>
         </Layout.Container>
       </Layout>
